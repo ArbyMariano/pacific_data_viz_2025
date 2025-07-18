d6e9f122e1d90238105e45658fe76f377385b70d
Arby Mariano <arbyamariano@gmail.com>
2025-07-18 16:39:29 -0700

    Clean variable names

--- code/data_wrangling.R
+++ code/data_wrangling.R
@@ -1,12 +1,37 @@
-# Install packages if you don't have it
+# Install packages if you don't have it -----------------------------------
 
 # install.packages("tidyverse")
+# install.packages("janitor")
 
-# Load packages
+# Load packages -----------------------------------------------------------
 
 library(tidyverse)
+library(janitor)
 
-# Import data
+# Import data -------------------------------------------------------------
 
-read_csv("data_raw/SPC,DF_BP50_2,1.0+all.csv") |> 
-  view()
+bp2050 <- read_csv("data_raw/bp2050.csv") |> 
+  clean_names()
+
+# Clean variables names ---------------------------------------------------
+
+bp2050 <- bp2050 |> 
+  rename(action_id = action,
+         freq_id = freq,
+         indicator_id = indicator,
+         sex_id = sex,
+         age_id = age,
+         urbanization_id = urbanization,
+         income_id = income,
+         education_id = education,
+         occupation_id = occupation,
+         composite_breakdown_id = composite_breakdown,
+         disability_id = disability,
+         unit_measure_id = unit_measure,
+         reporting_type_id = reporting_type,
+         nature_id = nature
+         )
+
+bp2050 <- bp2050 |> 
+  rename_with(~ gsub("_2","_label", .x, fixed = TRUE))
+
